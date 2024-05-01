aadf4e47c301894e692ec5179462a88fb3fc73c6
Tasha A. NKEH <tashankeh@gmail.com>
2024-05-01 20:12:12 +0100

    updated configs in the locallibrary.settings and catalog.urls

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
 from django.views.generic import RedirectView
 from django.conf.urls.static import static
@@ -23,7 +24,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("catalog/", include("catalog.urls")),
-    path("", RedirectView.as_view(url="catalog/", permanemt =True)),
-    static(settings.STATIC_URL, document_root.STATIC_ROOT)
+    path("", RedirectView.as_view(url="catalog/", permanent =True)),
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
